af3982909750f4ddb2356d6ca7fcda2ce7438aa4
Anton Mtz <antonio_halo1@hotmail.com>
2025-02-13 15:45:00 -0600

    Se creo la funcion que alberga el menu principal del programa y se valido que la opcion ingresada por el usuario sea valida o no

--- Administrador_de_recetas.py
+++ Administrador_de_recetas.py
@@ -2,6 +2,7 @@
 from pathlib import Path
 
 total_de_recetas = 0
+opciones_del_menu = 0
 
 print("Bienvenido al administrador de recetas\n")
 
@@ -14,5 +15,33 @@ print("Todas las categorías y recetas que vayas creando se almacenan en la sigu
 for txt in Path(carpeta_base).glob("**/*.txt"):
     total_de_recetas = total_de_recetas + 1
 
-print(f"\nLa cantidad de recetas almancenadas (entre todas las categorías) es de: {total_de_recetas}")
+print(f"\nLa cantidad de recetas almancenadas (entre todas las categorías) es de: {total_de_recetas}\n")
+
+#Esta funcion es el menu principal del programa y realiza una tarea determinada
+#dependiendo de la opcion ingresada por el usuario
+def menu_principal(opcion_ingresada):
+    while opcion_ingresada != 6:
+        print("Opciones del menú\n"
+              "1. Leer recetas\n"
+              "2. Crear receta\n"
+              "3. Crear categoría\n"
+              "4. Eliminar receta\n"
+              "5. Eliminar categoría\n"
+              "6. Finalizar programa\n")
+
+        try:
+            opcion_ingresada = int(input("Elige la opción que desees: "))
+        except:
+            print("Has ingresado un valor no valido. Favor de ingresar una opción valida.\n")
+            opcion_ingresada = 0
+
+    return "Programa finalizado..."
+
+
+menu_principal(opciones_del_menu)
+
+
+
+
+
 
