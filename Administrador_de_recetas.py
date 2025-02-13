3a3db1f3c68079659895caf66ffb3be6550507ef
Anton Mtz <antonio_halo1@hotmail.com>
2025-02-12 18:21:58 -0600

    Se agrego el for y el mensaje con los que le mostramos al usuario cuantas recetas hay en total (de todas las categorias)

--- Administrador_de_recetas.py
+++ Administrador_de_recetas.py
@@ -1,12 +1,18 @@
 #Proyecto 'administrador de recetas'
 from pathlib import Path
 
+total_de_recetas = 0
+
 print("Bienvenido al administrador de recetas\n")
 
-#Todos las carpetas y archivos que vayamos creando se almacenan aquí
+#Todas las carpetas y archivos que vayamos creando se almacenan en esta ruta
 carpeta_base = Path(Path.home(), "Recetas")
 
 print("Todas las categorías y recetas que vayas creando se almacenan en la siguiente ruta: ", carpeta_base)
 
+#Con este for recorremos todas las sub carpetas de la carpeta base y buscamos todos los archivos txt hay
+for txt in Path(carpeta_base).glob("**/*.txt"):
+    total_de_recetas = total_de_recetas + 1
 
+print(f"\nLa cantidad de recetas almancenadas (entre todas las categorías) es de: {total_de_recetas}")
 
