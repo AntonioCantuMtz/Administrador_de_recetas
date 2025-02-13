bd9fb306acb0d0de35739d087f9c0befce69103c
Anton Mtz <antonio_halo1@hotmail.com>
2025-02-12 18:01:13 -0600

    Se agrego el mensaje de bienvenida y el mensaje donde aparece la ruta donde se iran almacenando los archivos creados

--- Administrador_de_recetas.py
+++ Administrador_de_recetas.py
@@ -1,3 +1,12 @@
-#Proyecto administrador de recetas
-print("Hola :D")
+#Proyecto 'administrador de recetas'
+from pathlib import Path
+
+print("Bienvenido al administrador de recetas\n")
+
+#Todos las carpetas y archivos que vayamos creando se almacenan aquí
+carpeta_base = Path(Path.home(), "Recetas")
+
+print("Todas las categorías y recetas que vayas creando se almacenan en la siguiente ruta: ", carpeta_base)
+
+
 
